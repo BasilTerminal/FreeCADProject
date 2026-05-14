44faa3c028c00719a9a24f11775efdc54afe8d4e
Basil <terminal2014start@gmail.com>
2026-05-14 23:34:56 +0300

    Update pult.ino

--- Черній/PULT/pult/pult.ino
+++ Черній/PULT/pult/pult.ino
@@ -9,32 +9,53 @@ const int8_t RFin = 8;
 
 static void (*STATE)();
 
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(8, 11);  // RX, TX
+
 
 void setup() {
   Serial.begin(115200);
-  STATE = &state01;
+
+
+  mySerial.begin(1200);
+  mySerial.println("Hello, world?");
+
 
   pinMode(Rele, OUTPUT);
   pinMode(Buzer, OUTPUT);
   pinMode(BuzerLed, OUTPUT);
+  pinMode(RFin, INPUT);
 
   digitalWrite(Rele, HIGH);
   delay(500);
   digitalWrite(Rele, LOW);
 
   digitalWrite(Buzer, HIGH);
-  delay(500);
+  delay(200);
   digitalWrite(Buzer, LOW);
 
   digitalWrite(BuzerLed, HIGH);
-  delay(500);
+  delay(200);
   digitalWrite(BuzerLed, LOW);
+
+
+  STATE = &state01;
 }
 
 void loop() {
 
+  //if (mySerial.available()) {
+
+    //char a=mySerial.read();
+
+
+    Serial.println(mySerial.available());//(a, HEX);
+    mySerial.println(0x55);//(a, HEX);
+  //}
+
   (*STATE)();
-  delay(200);
+  delay(100);
 }
 
 // state01. Очікуємо натискання кнопки пульта
@@ -44,7 +65,6 @@ void state01() {
 
   //if (currentOfCharging() > 0) {
   STATE = &state02;
-
 }
 
 // state02.
@@ -52,7 +72,6 @@ void state01() {
 void state02() {
 
   STATE = &state01;
-
 }
 
 
