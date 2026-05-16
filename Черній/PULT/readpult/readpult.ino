01810717192ae8260c63cc21e5cb4f5a429d5b0f
Basil <terminal2014start@gmail.com>
2026-05-16 20:11:21 +0300

    g

--- Черній/PULT/readpult/readpult.ino
+++ Черній/PULT/readpult/readpult.ino
@@ -1,19 +1,26 @@
 #include <stdlib.h>
-#include <string.h>
 
 const int8_t Rele = 2;
 const int8_t Buzer = 3;
 const int8_t BuzerLed = 4;
 const int8_t RFin = 8;
 
-const char pult1cl[] = { 0x2, 0x2E, 0x62, 0xF9, 0x49, 0xCB, 0xF9, 0x4B };
-const char pult1op[] = { 0x2, 0x2E, 0x62, 0xF9, 0x49, 0xCB, 0xF9, 0x79 };
-const char pult2cl[] = { 0x2, 0x22, 0x2E, 0xFB, 0xFB, 0x49, 0xF9, 0x4B };
-const char pult2op[] = { 0x2, 0x22, 0x2E, 0xFB, 0xFB, 0x49, 0xF9, 0x79 };
+const char key1[]={0x2,0x2E,0x62,0xF9,0x49,0xCB,0xF9,0x4B};
+const char key2[]={0x2,0x2E,0x62,0xF9,0x49,0xCB,0xF9,0x79};
+const char key3[]={0x2,0x22,0x2E,0xFB,0xFB,0x49,0xF9,0x4B};
+const char key4[]={0x2,0x22,0x2E,0xFB,0xFB,0x49,0xF9,0x79};
 
-char inbuf[16] = "";
+
+char inbuf[16]="";
+
+
+
+
+
+//const int16_t VoltageLevelWhenBatteryPluged = 900;
 
 static void (*STATE)();
+
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(8, 11);  // RX, TX
@@ -21,7 +28,10 @@ SoftwareSerial mySerial(8, 11);  // RX, TX
 
 void setup() {
   Serial.begin(115200);
+
+
   mySerial.begin(2400);
+
 
   pinMode(Rele, OUTPUT);
   pinMode(Buzer, OUTPUT);
@@ -32,43 +42,38 @@ void setup() {
   delay(500);
   digitalWrite(Rele, LOW);
 
-  digitalWrite(Buzer, HIGH);
-  delay(100);
-  digitalWrite(Buzer, LOW);
-
   digitalWrite(BuzerLed, HIGH);
-  delay(100);
+  delay(50);
   digitalWrite(BuzerLed, LOW);
 
 
   STATE = &state01;
 
-  mySerial.setTimeout(10);
+  mySerial.setTimeout(5);
 }
 
 void loop() {
-  char* p;
 
-
-  if (mySerial.available() > 16) {
+  if (mySerial.available() > 32) {
 
     //char a=mySerial.read();
 
-    for (byte i = 0; i < 16; i++) {
-      inbuf[i] = mySerial.read();
-        }
 
-    p = strstr(inbuf, pult1op);
-    if (p != NULL) { Serial.println("Pult 1 open"); }
 
-    p = strstr(inbuf, pult2op);
-    if (p != NULL) { Serial.println("Pult 2 open"); }
+      //Serial.print(F("0x"));
+      Serial.print(mySerial.read(), HEX);
+      Serial.print(F(","));
 
-    p = strstr(inbuf, pult1cl);
-    if (p != NULL) { Serial.println("Pult 1 close"); }
 
-    p = strstr(inbuf, pult2cl);
-    if (p != NULL) { Serial.println("Pult 2 close"); }
+    //String s = mySerial.readString();
+    //Serial.println(s);
+    //Serial.println(key1);
+
+
+
+
+    Serial.println();
+    //mySerial.println(0x55);  //(a, HEX);
   }
 
   //(*STATE)();
