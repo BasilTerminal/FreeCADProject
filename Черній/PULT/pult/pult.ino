ecff23169ea1537367d77a8884117c39367fc557
Basil <terminal2014start@gmail.com>
2026-05-17 18:05:05 +0300

    Update pult.ino

--- Черній/PULT/pult/pult.ino
+++ Черній/PULT/pult/pult.ino
@@ -11,16 +11,18 @@ const char pult1op[] = { 0x2E, 0x62, 0xF9, 0x49, 0xCB, 0xF9, 0x79, 0 };
 const char pult2cl[] = { 0x22, 0x2E, 0xFB, 0xFB, 0x49, 0xF9, 0x4B, 0 };
 const char pult2op[] = { 0x22, 0x2E, 0xFB, 0xFB, 0x49, 0xF9, 0x79, 0 };
 
+#define SIZEBUF 16             //розмір вхідний буфер
+char inbuf[SIZEBUF] = "";      //вхідний буфер
+unsigned int NumRepeat = 0;    // лічильник повторів
+unsigned long prevMillis = 0;  // час останньої зміни стану
+const long interval = 200;    // інтервал між настискання і відпусканням
+unsigned long currMillis = 0;
 
 
 
+static void (*STATE)();  // автомат станів
 
-#define SIZEBUF 16
-char inbuf[SIZEBUF] = "";
-
-static void (*STATE)();
 #include <SoftwareSerial.h>
-
 SoftwareSerial mySerial(8, 11);  // RX, TX
 
 
@@ -33,10 +35,6 @@ void setup() {
   pinMode(BuzerLED, OUTPUT);
   pinMode(RFin, INPUT);
 
-  // digitalWrite(Rele, HIGH);
-  // delay(500);
-  // digitalWrite(Rele, LOW);
-
   sound1();
   sound2();
 
@@ -46,6 +44,14 @@ void setup() {
 }
 
 void loop() {
+  (*STATE)();
+  //delay(100);
+}
+
+// state01. Очікуємо натискання кнопки пульта
+// Розпізнаємо кнопку і реагуємо
+// Після подачі живлення ми тут
+void state01() {
   char* p;
 
   if (mySerial.available() > SIZEBUF) {
@@ -54,50 +60,72 @@ void loop() {
     }
 
     p = strstr(inbuf, pult1op);
-    if (p) { sound1(); Serial.println("Pult 1 open"); }
-
+    if (p) {
+      NumRepeat++;
+      prevMillis = millis();
+      STATE = &state02;
+      return;
+    }
     p = strstr(inbuf, pult2op);
-    if (p) { sound1(); Serial.println("Pult 2 open"); }
+    if (p) {
+      NumRepeat++;
+      prevMillis = millis();
+      STATE = &state02;
+      return;
+    }
 
     p = strstr(inbuf, pult1cl);
-    if (p) { sound2(); Serial.println("Pult 1 close"); }
-
+    if (p) {
+      STATE = &state03;
+      return;
+    }
     p = strstr(inbuf, pult2cl);
-    if (p) { sound2(); Serial.println("Pult 2 close"); }
+    if (p) {
+      STATE = &state03;
+      return;
+    }
   }
 
-  //(*STATE)();
-  //delay(100);
-}
 
-// state01. Очікуємо натискання кнопки пульта
-// Розпізнаємо кнопку і реагуємо
-// Після подачі живлення ми тут
-void state01() {
 
-  //if (currentOfCharging() > 0) {
-  STATE = &state02;
+  if (millis() - prevMillis >= interval) {
+    prevMillis == 0;
+    NumRepeat = 0;
+  }
+
+  STATE = &state01;
 }
 
 
 
 // state02.
 // очікуємо натискання на К1 протягом 5 сек
+//open
 void state02() {
 
+  sound1();
+  Serial.print(NumRepeat);
+  Serial.println(" Pult - open");
+  if (prevMillis == 0) prevMillis = millis();  // перший раз написнули
   STATE = &state01;
 }
 
+//close
+void state03() {
+  sound2();
+  Serial.println("Pult - close");
+  STATE = &state01;
+}
 
 void sound1() {
   digitalWrite(Buzer, HIGH);
-  delay(40);
+  delay(20);
   digitalWrite(Buzer, LOW);
 }
 
 void sound2() {
   digitalWrite(BuzerLED, HIGH);
-  delay(40);
+  delay(20);
   digitalWrite(BuzerLED, LOW);
 }
 
