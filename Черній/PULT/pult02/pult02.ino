b4bc4909aeba98aaf94d3c7cff915a774a034dfb
BasilTerminal <terminal2014start@gmail.com>
2026-05-18 17:27:09 +0300

    p

--- Черній/PULT/pult02/pult02.ino
+++ Черній/PULT/pult02/pult02.ino
@@ -17,7 +17,11 @@ unsigned int NumRepeat = 0;    // лічильник повторів
 unsigned long prevMillis = 0;  // час останньої зміни стану
 const long interval = 200;     // інтервал між настискання і відпусканням
 unsigned long currMillis = 0;
-bool flag_wait = 0;  // флаг що чекаємо кнопку
+bool flag_wait = 0;   // флаг що чекаємо кнопку
+bool flag_close = 0;  //
+bool flag_open = 0;   //
+
+
 //unsigned long beep_prevMs = 0;   // для біпера
 //unsigned long beep_curMs = 0;    // для біпера
 
@@ -30,7 +34,8 @@ static void (*STATE)();  // автомат станів
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(8, 11);  // RX, TX
 
-GTimer<millis> tmrWait; // чекаємо 5 скунд натискання кнопки
+GTimer<millis> tmrWait;              // чекаємо 5 скунд натискання кнопки
+GTimer<millis> tmrFlash(500, true);  // для блимання
 
 void setup() {
   Serial.begin(115200);
@@ -51,85 +56,74 @@ void setup() {
 
   tmrWait.setMode(GTMode::Timeout);
   tmrWait.setTime(5000);
-  tmrWait.start();
 }
 
 void loop() {
-  //tmrWait.tick();
-  // контролюємо таймери
-  if (!tmrWait) {
-    //Serial.println("timeout");
-    EVERY_MS(1000) {
-      //Serial.println("beep");
-      //digitalWrite(BuzerLED, !digitalRead(BuzerLED));
-      digitalWrite(Rele, !digitalRead(Rele));
-    }
-  }
 
   (*STATE)();
 }
 
-// state01. Очікуємо натискання кнопки пульта
-// Розпізнаємо кнопку і реагуємо
+// =================== state01 ====================
+// Очікуємо натискання кнопки пульта
+// Розпізнаємо і реагуємо
 // Після подачі живлення ми тут
 void state01() {
-  char* p;
+  flag_close = 0;
+  flag_open = 0;
 
   if (mySerial.available() > SIZEBUF) {
     for (byte i = 0; i < SIZEBUF; i++) {
       inbuf[i] = mySerial.read();
     }
-
-    p = strstr(inbuf, pult1op);
-    if (p) {
-      
-      NumRepeat++;
-      prevMillis = millis();
-      STATE = &state02;
-      return;
-    }
-    p = strstr(inbuf, pult2op);
-    if (p) {
-      
-      NumRepeat++;
-      prevMillis = millis();
-      STATE = &state02;
-      return;
-    }
-
-    p = strstr(inbuf, pult1cl);
-    if (p) {
-      STATE = &state03;
-      return;
-    }
-    p = strstr(inbuf, pult2cl);
-    if (p) {
-      STATE = &state03;
-      return;
-    }
+    if (strstr(inbuf, pult1op)) flag_open = 1;
+    if (strstr(inbuf, pult2op)) flag_open = 1;
+    if (strstr(inbuf, pult1cl)) flag_close = 1;
+    if (strstr(inbuf, pult2cl)) flag_close = 1;
   }
 
-  if (millis() - prevMillis >= interval) {
-    prevMillis == 0;
-    NumRepeat = 0;
+  if (flag_open) {
+    tmrWait.start();
+    NumRepeat++;
+    STATE = &state02;
+    return;
   }
+
+
+  if (flag_close) {
+    STATE = &state03;
+    return;
+  }
+
+
+  // if (millis() - prevMillis >= interval) {
+  //   prevMillis == 0;
+  //   NumRepeat = 0;
+  // }
 
   STATE = &state01;
 }
 
 
-
-// state02.
+//================== state02 ===========================
 // очікуємо натискання на К1 протягом 5 сек
 //open
 void state02() {
+  //блимаємо
+  if (tmrFlash) digitalWrite(BuzerLED, !digitalRead(BuzerLED));
 
-  //  sound1();
+  //чекаємо настискання K1
+  //.......  
 
-  Serial.print(NumRepeat);
-  Serial.println(" Pult - open");
-  if (prevMillis == 0) prevMillis = millis();  // перший раз написнули
-  STATE = &state01;
+  // Serial.print(NumRepeat);
+  //Serial.println(" Pult - open");
+  
+  if (tmrWait) {  // час очікування вийшов
+    Stop();
+    digitalWrite(BuzerLED, LOW);
+    STATE = &state01;
+    return;
+  }
+  STATE = &state02;
 }
 
 
@@ -174,15 +168,4 @@ void Stop() {
   sound1();
   sound1();
   sound1();
-}
-
-
-void updateIndicationByInnerState() {
-  Serial.print(F("cur: "));
-  //Serial.print(currentOfCharging());
-  Serial.print(F("\tconn: "));
-  //Serial.print(connectedBattery());
-  Serial.print(F("\tstate: "));
-  //Serial.print(STATE == &state01 ? 1 : 2);
-  Serial.println("");
 }
